cf980fa367a36947030500ead395a68bf183bf83
kruna <softoperstechnologies@gmail.com>
2018-06-04 12:36:03 +0530

    - added fcm

---
+++ app/src/main/java/com/interview/omnifyinterview/fcm/MyFirebaseInstanceIDService.java
@@ -0,0 +1,18 @@
+package com.interview.omnifyinterview.fcm;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseInstanceIDService";
+
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+    }
+}
---
+++ app/src/main/java/com/interview/omnifyinterview/fcm/MyFirebaseMessagingService.java
@@ -0,0 +1,26 @@
+package com.interview.omnifyinterview.fcm;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyFirebaseMessagingServ";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+    }
+}
