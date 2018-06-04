6038c23bee31debffabc037555d617679ca9c17d
kruna <softoperstechnologies@gmail.com>
2018-06-04 12:55:34 +0530

    - added custom notification.

--- app/src/main/java/com/interview/omnifyinterview/fcm/MyFirebaseMessagingService.java
+++ app/src/main/java/com/interview/omnifyinterview/fcm/MyFirebaseMessagingService.java
@@ -1,9 +1,18 @@
 package com.interview.omnifyinterview.fcm;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.interview.omnifyinterview.R;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
@@ -21,6 +30,54 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            String body = remoteMessage.getNotification().getBody();
+            String title = remoteMessage.getNotification().getTitle();
+
+            int currentApiVersion = Build.VERSION.SDK_INT;
+            if (currentApiVersion >= Build.VERSION_CODES.O) {
+                sendNotificationChannel(title, body);
+            } else {
+                sendNotificationOld(title, body);
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void sendNotificationChannel(String title, String body) {
+
+        String name = "Story Updates";
+        int notificationId = 1;
+        NotificationChannel updateChannel = new NotificationChannel(name
+                , "Update channel"
+                , NotificationManager.IMPORTANCE_HIGH);
+
+        updateChannel.setLightColor(Color.GREEN);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (notificationManager != null) {
+            notificationManager.createNotificationChannel(updateChannel);
+            Notification notification = new Notification.Builder(getApplicationContext())
+                    .setContentTitle(title)
+                    .setContentText(body)
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setChannelId(name)
+                    .build();
+
+            notificationManager.notify(notificationId, notification);
+        }
+    }
+
+    private void sendNotificationOld(String title, String body) {
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext())
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(body));
+
+        NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        if (notificationManager != null) {
+            notificationManager.notify(1, mBuilder.build());
         }
     }
 }
